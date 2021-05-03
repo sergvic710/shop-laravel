4635971afe359048ea421cad29cc6ffb48611665
Sergei <sergvic@yandex.ru>
2021-05-03 22:07:57 +0300

    Api list products and tags

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $tags = Product::select('*')->get();
+        return response()->json($tags);
+    }
 }
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tag;
 
 class TagController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $tags = Tag::select('*')->get();
+        return response()->json($tags);
+    }
 }
